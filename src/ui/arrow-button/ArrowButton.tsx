3fff727a36bdfac68d43d230f703b82d9d327186
Ekaterina Verbilova <cverbilova@gmail.com>
2025-11-23 19:45:24 +0300

    sending for review

--- src/ui/arrow-button/ArrowButton.tsx
+++ src/ui/arrow-button/ArrowButton.tsx
@@ -3,7 +3,7 @@ import arrow from 'src/images/arrow.svg';
 import styles from './ArrowButton.module.scss';
 import clsx from 'clsx';
 
-/** Функция для обработки открытия/закрытия формы */
+/** Открытие/закрытие формы */
 export type OnClick = () => void;
 
 type ArrowButtonProps = {
@@ -13,7 +13,7 @@ type ArrowButtonProps = {
 
 export const ArrowButton = ({ isOpen, onClick }: ArrowButtonProps) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
+		/* role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
